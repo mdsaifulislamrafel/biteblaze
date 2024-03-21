2b77a57d8441499e993653d5fff189dc5e9e7115
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-03-21 16:52:26 +0600

    navbar section start

---
+++ src/components/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../../Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      {/* Navbar start */}
+      <Navbar />
+      <h1>Navbar</h1>
+      {/* Navbar end */}
+    </div>
+  );
+};
+
+Home.propTypes = {};
+
+export default Home;
